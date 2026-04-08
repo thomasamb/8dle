4ea1ab00e1895da297dee8b75f7eb5d25d5718cf
Thomas Arnold <tarnoldamb@gmail.com>
2026-04-07 17:49:18 -0700

    Data init

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div>
-      <Image
-        src="/assets/MarioKartStadium/MarioKartStadium.png"
-        alt="Mario Kart Stadium"
-        width={200}
-        height={100}
-      />
-    </div>
-  );
+  return <div></div>;
 }
--- app/ui/game.tsx
+++ app/ui/game.tsx
@@ -1,7 +1,14 @@
+import Search from "./search";
+
 export default function Game() {
   return (
     <>
       <p>Game</p>
+      <RoundTracker />
     </>
   );
+}
+
+function RoundTracker() {
+    return <><p>Round</p></>
 }

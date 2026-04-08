05cee46cfa583b3da48b6154ffdf1378d4fee839
Thomas Arnold <tarnoldamb@gmail.com>
2026-04-07 17:20:37 -0700

    More init

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <Image
+        src="/assets/MarioKartStadium/MarioKartStadium.png"
+        alt="Mario Kart Stadium"
+        width={200}
+        height={100}
+      />
+    </div>
+  );
 }

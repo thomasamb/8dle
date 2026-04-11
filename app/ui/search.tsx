73bdbdb13554a1599aaa3bc662c4c2d7043e218a
Thomas Arnold <tarnoldamb@gmail.com>
2026-04-11 15:01:21 -0700

    Structure of passing in props to children components in game.tsx

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,4 +1,12 @@
-export default function Search() {
+import GameState from "../lib/gameState";
+
+export default function Search({
+  gameState,
+  setGameState,
+}: {
+  gameState: GameState;
+  setGameState: React.Dispatch<React.SetStateAction<GameState>>;
+}) {
   return (
     <>
       <p>Search</p>

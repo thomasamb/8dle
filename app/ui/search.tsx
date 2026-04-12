4eeda742131696b02d84b6acdeada9abb892a75d
Thomas Arnold <tarnoldamb@gmail.com>
2026-04-12 12:28:14 -0700

    Search and round progression

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,15 +1,32 @@
 import GameState from "../lib/gameState";
+import { useActionState } from "react";
+import GameHandler from "../lib/gameHandler";
 
 export default function Search({
   gameState,
   setGameState,
+  gameHandler,
 }: {
   gameState: GameState;
   setGameState: React.Dispatch<React.SetStateAction<GameState>>;
+  gameHandler: GameHandler;
 }) {
+  const [state, formAction] = useActionState(
+    (previousState: GameState, formData: FormData) => {
+      const guess = formData.get("guess") as string;
+      const result: GameState = gameHandler.submitGuess(guess);
+      setGameState(result);
+      console.log(gameState);
+      return result;
+    },
+    gameState,
+  );
   return (
     <>
-      <p>Search</p>
+      <form action={formAction}>
+        <input type="text" name="guess" />
+        <button type="submit">Guess</button>
+      </form>
     </>
   );
 }

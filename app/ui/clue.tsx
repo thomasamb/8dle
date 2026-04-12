13e1dc46ea79f8f23a098419f0a8c8522fb89ef9
Thomas Arnold <ybn4aq@virginia.edu>
2026-04-12 14:22:37 -0700

    RoundTracker and clues render and stack vertically

--- app/ui/clue.tsx
+++ app/ui/clue.tsx
@@ -1,50 +1,68 @@
 import { useState, useEffect } from "react";
 import GameState from "../lib/gameState";
 import Image from "next/image";
-import { Pagination } from "react-bootstrap";
+import { ReactNode } from "react";
 
 export default function Clue({ gameState }: { gameState: GameState }) {
-  let active = 1;
-  function roundSwitch(round: number) {
-    switch (round) {
-      case 1:
-        return (
-          <div id="round1">
-            <Image
-              src={gameState.answer.trackLayoutPath}
-              alt="Track layout"
-              height={100}
-              width={100}
-            />
-          </div>
-        );
-      case 2:
-        return (
-          <div id="round2">
-            <Image
-              src={gameState.answer.screenshotPath}
-              alt="Track screenshot"
-              height={100}
-              width={100}
-            />
-          </div>
-        );
-      case 3:
-        return (
-          <div id="round3">
-            <Image
-              src={gameState.answer.cupImagePath}
-              alt={gameState.answer.cup}
-              height={100}
-              width={100}
-            />
-          </div>
-        );
-      case 4:
-        return TrackSong({ gameState });
+  const Round1 = (): ReactNode => {
+    return (
+      <div id="round1">
+        <Image
+          src={gameState.answer.trackLayoutPath}
+          alt="Track layout"
+          height={100}
+          width={100}
+        />
+      </div>
+    );
+  };
+
+  const Round2 = (): ReactNode => {
+    return (
+      <div id="round2">
+        <Image
+          src={gameState.answer.screenshotPath}
+          alt="Track screenshot"
+          height={100}
+          width={100}
+        />
+      </div>
+    );
+  };
+
+  const Round3 = (): ReactNode => {
+    return (
+      <div id="round3">
+        <Image
+          src={gameState.answer.cupImagePath}
+          alt={gameState.answer.cup}
+          height={100}
+          width={100}
+        />
+      </div>
+    );
+  };
+
+  const Round4 = (): ReactNode => {
+    return TrackSong({ gameState });
+  };
+
+  function renderRound(idx: number): ReactNode {
+    const roundComponents = [Round1, Round2, Round3, Round4];
+    if (idx <= gameState.round) {
+      const RoundComponent = roundComponents[idx - 1];
+      return <RoundComponent />;
     }
   }
-  return <div id="clue">{roundSwitch(gameState.round)}</div>;
+
+  return (
+    <div id="clues">
+      {renderRound(1)}
+      {renderRound(2)}
+      {renderRound(3)}
+      {renderRound(4)}
+    </div>
+  );
 }
 
 function TrackSong({ gameState }: { gameState: GameState }) {

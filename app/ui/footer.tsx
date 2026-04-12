d38bd7013441b541a69604f15f9adad3e0ea2569
Thomas Arnold <tarnoldamb@gmail.com>
2026-04-12 15:54:16 -0700

    footer

--- app/ui/footer.tsx
+++ app/ui/footer.tsx
@@ -1,7 +1,12 @@
 export default function Footer() {
   return (
-    <>
-      <p>Footer</p>
-    </>
+    <div id="footerContainer">
+      <p>© 2026 Thomas Arnold, All Rights Reserved.</p>
+      <p>
+        Mario Kart 8 Deluxe and all related content are trademarks of Nintendo.
+        This site is not affiliated with or endorsed by Nintendo. This site is a
+        non-commercial project.
+      </p>
+    </div>
   );
 }

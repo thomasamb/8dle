d541c96b82430387d6066e6be4c3cdb8e50cef7b
Thomas Arnold <tarnoldamb@gmail.com>
2026-04-12 18:22:32 -0700

    Changed roundTracker to use hearts and rearranged some game components

--- app/ui/roundTracker.tsx
+++ app/ui/roundTracker.tsx
@@ -1,22 +1,22 @@
 import GameState from "../lib/gameState";
-import { FaRegCircle } from "react-icons/fa"; // hollow circle
-import { FaCircle } from "react-icons/fa"; // opaque circle
+import { FaHeart } from "react-icons/fa6";
+import { FaRegHeart } from "react-icons/fa6";
 
 export default function RoundTracker({ gameState }: { gameState: GameState }) {
-  const opaqueCircle = <FaCircle color="blue" />;
-  const hollowCircle = <FaRegCircle />;
-  const circle = (idx: number) => {
-    if (idx <= gameState.round) {
-      return opaqueCircle;
+  const filledHeart = <FaHeart />;
+  const emptyHeart = <FaRegHeart />;
+  const heart = (idx: number) => {
+    if (idx < gameState.round) {
+      return emptyHeart;
     }
-    return hollowCircle;
+    return filledHeart;
   };
   return (
     <div id="roundTracker" className="d-flex gap-2">
-      <span>{circle(1)}</span>
-      <span>{circle(2)}</span>
-      <span>{circle(3)}</span>
-      <span>{circle(4)}</span>
+      <span>{heart(1)}</span>
+      <span>{heart(2)}</span>
+      <span>{heart(3)}</span>
+      <span>{heart(4)}</span>
     </div>
   );
 }
